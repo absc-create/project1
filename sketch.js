9453c0f500baca1baf8de89e05b4da7140eeacb4
absc-create <77611090+absc-create@users.noreply.github.com>
2022-03-23 14:53:08 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,40 @@
+var box;
+
+function setup() {
+  createCanvas(666,666);
+  box = createSprite(200,200,333,333);
+
+}
+
+function draw() 
+{
+   background("purple");
+
+  if (keyIsDown(RIGHT_ARROW)) 
+  {
+    box.position.x = box.position.x +5;
+  }
+
+    if (keyIsDown(LEFT_ARROW)) 
+  {
+    box.position.x= box.position.x -5;
+    
+  }
+ 
+    if (keyIsDown(UP_ARROW)) 
+  {
+    box.position.y = box.position.y - 5;
+   
+  }
+
+  if (keyIsDown(DOWN_ARROW)) 
+  {
+    box.position.y = box.position.y + 5;
+  }
+
+  drawSprites();
+}
+
+
+
+
